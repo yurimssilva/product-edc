856bf6528e9dbc98701150f972c6d923f327e86b
Dominik Pinsel <dominik.pinsel@daimler.com>
2023-01-03 14:30:38 +0100

    wip
    Signed-off-by: Dominik Pinsel <dominik.pinsel@daimler.com>

--- edc-extensions/ssi/src/test/java/org/eclipse/tractusx/ssi/fakes/VerifiableCredentialStoreFake.java
+++ edc-extensions/ssi/src/test/java/org/eclipse/tractusx/ssi/fakes/VerifiableCredentialStoreFake.java
@@ -22,47 +22,22 @@ public class VerifiableCredentialStoreFake implements VerifiableCredentialStore 
     }
 
     public void prepareMembershipCredential() {
-        var subject = CredentialSubject.builder()
-                .context(URI.create("https://raw.githubusercontent.com/catenax-ng/product-core-schemas/main/businessPartnerData"))
-                .id(URI.create("urn:test:bpn/subject"))
-                .type("MembershipCredential")
-                .claims(Map.of("memberOf", "MembershipCredential",
-                        "status", "Active",
-                        "startTime", new Date().toString()))
+        Map<String, Object> claims = new LinkedHashMap<>();
+        claims.put("type", "MembershipCredential");
+        claims.put("status", "active");
+        CredentialSubject credentialSubject = CredentialSubject.builder()
+                .id(URI.create("did:example:ebfeb1f712ebc6f1c276e12ec21"))
+                .claims(claims)
                 .build();
+
         membershipCredential = VerifiableCredential.builder()
                 .id(URI.create("urn:test:bpn/1"))
-                //.contexts(List.of(URI.create("https://www.w3.org/2018/credentials/v1"), URI.create("https://raw.githubusercontent.com/catenax-ng/product-core-schemas/main/businessPartnerData")))
-                .context(VerifiableCredentialContexts.JSONLD_CONTEXT_W3C_2018_CREDENTIALS_EXAMPLES_V1)
+                .contexts(List.of(URI.create("https://www.w3.org/2018/credentials/v1"), URI.create("https://www.w3.org/2018/credentials/examples/v1")))
                 .types(List.of("MembershipCredential", "VerifiableCredential"))
                 .issuanceDate(new Date())
                 .expirationDate(new Date(2024, 1, 1))
                 .issuer(URI.create("urn:test:bpn"))
-                .credentialSubject(subject)
+                .credentialSubject(credentialSubject)
                 .build();
-
-//        Map<String, Object> claims = new LinkedHashMap<>();
-//        Map<String, Object> degree = new LinkedHashMap<String, Object>();
-//        degree.put("name", "Bachelor of Science and Arts");
-//        degree.put("type", "BachelorDegree");
-//        claims.put("college", "Test University");
-//        claims.put("degree", degree);
-//
-//        CredentialSubject credentialSubject = CredentialSubject.builder()
-//                .id(URI.create("did:example:ebfeb1f712ebc6f1c276e12ec21"))
-//                .claims(claims)
-//                .build();
-//        VerifiableCredential verifiableCredential = VerifiableCredential.builder()
-//                .context(VerifiableCredentialContexts.JSONLD_CONTEXT_W3C_2018_CREDENTIALS_EXAMPLES_V1)
-//                .type("UniversityDegreeCredential")
-//                .id(URI.create("http://example.edu/credentials/3732"))
-//                .issuer(URI.create("did:example:76e12ec712ebc6f1c221ebfeb1f"))
-//                .issuanceDate(JsonLDUtils.stringToDate("2019-06-16T18:56:59Z"))
-//                .expirationDate(JsonLDUtils.stringToDate("2019-06-17T18:56:59Z"))
-//                .credentialSubject(credentialSubject)
-//                .build();
-//
-//
-//        membershipCredential = verifiableCredential;
     }
 }
