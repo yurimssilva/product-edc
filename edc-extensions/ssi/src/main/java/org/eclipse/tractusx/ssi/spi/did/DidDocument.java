9e00d18cf1a061e1e624a1e3737479d378e3adaf
Maximilian Schmidt (ZF Friedrichshafen AG) <maximilian.schmidt.external@zf.com>
2023-01-13 13:18:12 +0100

    DID Doc needed fields

--- edc-extensions/ssi/src/main/java/org/eclipse/tractusx/ssi/spi/did/DidDocument.java
+++ edc-extensions/ssi/src/main/java/org/eclipse/tractusx/ssi/spi/did/DidDocument.java
@@ -5,4 +5,33 @@ import lombok.Value;
 @Value
 public class DidDocument {
     Did did;
+
+    /**
+     *     val id: String,
+     *     @SerialName("@context") @JsonProperty("@context")
+     *     val context: List<String>,
+     *     val alsoKnownAs: String? = null,
+     *     @Serializable(AnySerializer::class) val controller: Any? = null,
+     *     @SerialName("verificationMethod")
+     *     @JsonProperty("verificationMethod")
+     *     val verificationMethods: List<DidVerificationMethodDto>? = null,
+     *     @SerialName("authentication")
+     *     @JsonProperty("authentication")
+     *     val authenticationVerificationMethods: List<Any>? = null,
+     *     @SerialName("assertionMethod")
+     *     @JsonProperty("assertionMethod")
+     *     val assertionMethodVerificationMethods: List<Any>? = null,
+     *     @SerialName("keyAgreement")
+     *     @JsonProperty("keyAgreement")
+     *     val keyAgreementVerificationMethods: List<Any>? = null,
+     *     @SerialName("capabilityInvocation")
+     *     @JsonProperty("capabilityInvocation")
+     *     val capabilityInvocationVerificationMethods: List<Any>? = null,
+     *     @SerialName("capabilityDelegation")
+     *     @JsonProperty("capabilityDelegation")
+     *     val capabilityDelegationVerificationMethods: List<Any>? = null,
+     *     @SerialName("service")
+     *     @JsonProperty("service")
+     *     val services: List<DidServiceDto>? = null
+     */
 }
