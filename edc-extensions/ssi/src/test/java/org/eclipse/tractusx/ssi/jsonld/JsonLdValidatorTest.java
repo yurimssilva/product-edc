3b3ca12a0b30af4c0d5be3c8aed186df4ceef95a
Maximilian Schmidt (ZF Friedrichshafen AG) <maximilian.schmidt.external@zf.com>
2023-01-18 13:59:42 +0100

    wip JsonLd validation test

---
+++ edc-extensions/ssi/src/test/java/org/eclipse/tractusx/ssi/jsonld/JsonLdValidatorTest.java
@@ -0,0 +1,48 @@
+package org.eclipse.tractusx.ssi.jsonld;
+
+import com.apicatalog.jsonld.loader.DocumentLoader;
+import foundation.identity.jsonld.ConfigurableDocumentLoader;
+import foundation.identity.jsonld.JsonLDObject;
+import lombok.SneakyThrows;
+import org.eclipse.tractusx.ssi.extensions.core.jsonLd.JsonLdValidator;
+import org.eclipse.tractusx.ssi.extensions.core.jsonLd.JsonLdValidatorImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonLdValidatorTest {
+
+    private JsonLdValidator validator;
+
+    @BeforeEach
+    public void setUp(){
+      validator = new JsonLdValidatorImpl();
+    }
+
+    @Test
+    public void validateTestFail(){
+      JsonLDObject toTest = loadInvalidjsonLDObject();
+      boolean result = false;
+      String expectedException = "Undefined JSON-LD term: publicKeyMultibase";
+
+      Exception exception = Assertions.assertThrows(RuntimeException.class, () -> {
+        validator.validate(toTest);
+      });
+
+      Assertions.assertTrue(exception.getMessage().contains(expectedException));
+    }
+
+    @SneakyThrows
+    private JsonLDObject loadInvalidjsonLDObject(){
+      String didJsonLd = Files.readString(Paths.get(getClass().getClassLoader().getResource("./jsonld/invalidJsonLd.json").toURI()));
+      DocumentLoader loader = new ConfigurableDocumentLoader();
+      ((ConfigurableDocumentLoader) loader).setEnableHttps(true);
+      JsonLDObject object = JsonLDObject.fromJson(didJsonLd);
+      object.setDocumentLoader(loader);
+      return object;
+    }
+
+}
