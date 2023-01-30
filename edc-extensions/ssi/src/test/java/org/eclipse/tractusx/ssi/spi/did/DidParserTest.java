d76415a538361b98cd5e0c6b1ae97281254efc65
Maximilian Schmidt (ZF Friedrichshafen AG) <maximilian.schmidt.external@zf.com>
2023-01-30 14:45:07 +0100

    Added DidParser Unit-Test

--- edc-extensions/ssi/src/test/java/org/eclipse/tractusx/ssi/spi/did/DidParserTest.java
+++ edc-extensions/ssi/src/test/java/org/eclipse/tractusx/ssi/spi/did/DidParserTest.java
@@ -1,4 +1,78 @@
 package org.eclipse.tractusx.ssi.spi.did;
 
+import lombok.SneakyThrows;
+import org.eclipse.tractusx.ssi.extensions.core.exception.DidParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
 public class DidParserTest {
+
+  @Test
+  @SneakyThrows
+  public void parseUriSuccess(){
+    // given
+    String testDid = "did:web:www.someurl.com";
+    URI testUri = new URI(testDid);
+    // when
+    Did result = DidParser.parse(testUri);
+    // then
+    Assertions.assertTrue(result.toString().equals(testDid));
+    Assertions.assertTrue(result.toUri().equals(testUri));
+  }
+
+  @Test
+  @SneakyThrows
+  public void parseUriInvalidUri(){
+    // given
+    String expectedMessage = "URI is not a DID. URI:";
+    String testDid = "web:did";
+    URI testUri = new URI(testDid);
+    // when
+    DidParseException result = Assertions.assertThrows(DidParseException.class,
+            () -> DidParser.parse(testUri));
+    // then
+    Assertions.assertTrue(result.getMessage().contains(expectedMessage));
+  }
+
+  @Test
+  @SneakyThrows
+  public void parseUriInvalidLength(){
+    // given
+    String expectedMessage = "DID does not contain at least three parts";
+    String testDid = "did:web";
+    URI testUri = new URI(testDid);
+    // when
+    DidParseException result = Assertions.assertThrows(DidParseException.class,
+            () -> DidParser.parse(testUri));
+    // then
+    Assertions.assertTrue(result.getMessage().contains(expectedMessage));
+  }
+
+  @Test
+  @SneakyThrows
+  public void parseStringSuccess(){
+    // given
+    String testDid = "did:web:www.someurl.com";
+    URI testUri = new URI(testDid);
+    // when
+    Did result = DidParser.parse(testDid);
+    // then
+    Assertions.assertTrue(result.toString().equals(testDid));
+    Assertions.assertTrue(result.toUri().equals(testUri));
+  }
+
+  @Test
+  @SneakyThrows
+  public void parseStringInvalidString(){
+    // given
+    String expectedMessage = "Not able to create DID URI from string";
+    String testDid = "[]";
+    // when
+    DidParseException result = Assertions.assertThrows(DidParseException.class,
+            () -> DidParser.parse(testDid));
+    // then
+    Assertions.assertTrue(result.getMessage().contains(expectedMessage));
+  }
 }
